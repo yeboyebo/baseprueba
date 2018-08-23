e5b5f1b1e01e79b133de2c4c7e0cb4e21c1a888f
xavikortes <javiercortesveliz@gmail.com>
2018-08-23 11:53:01 +0200

    Update settings.py

--- AQNEXT/settings.py
+++ AQNEXT/settings.py
@@ -3,7 +3,7 @@ import sys
 
 PROJECT_ROOT = path.dirname(path.abspath(path.dirname(__file__)))
 
-sys.path.insert(0, path.join(PROJECT_ROOT, "../../motor/"))
+sys.path.insert(0, path.join(PROJECT_ROOT, "../../../motor/"))
 sys.path.insert(1, path.join(PROJECT_ROOT, "apps/"))
 
 try:
