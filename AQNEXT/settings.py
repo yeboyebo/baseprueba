f956c92bb30b4cfe1c39e6979948fbb0e6cf0902
xavikortes <javiercortesveliz@gmail.com>
2018-08-30 12:19:07 +0200

    Pasando importacion de urls a yeboapps.py

--- AQNEXT/settings.py
+++ AQNEXT/settings.py
@@ -6,24 +6,6 @@ PROJECT_ROOT = path.dirname(path.abspath(path.dirname(__file__)))
 sys.path.insert(0, path.join(PROJECT_ROOT, "../../../motor/"))
 sys.path.insert(1, path.join(PROJECT_ROOT, "apps/"))
 
-try:
-    from YBAQNEXT.settings import *
-    from YBWEB.ctxJSON import DICTJSON
-except ImportError as exc:
-    print(exc)
-
-
-YEBO_APPS = ()
-
-rest = open(path.join(PROJECT_ROOT, "config/urls.json")).read()
-oRest = DICTJSON.fromJSON(rest)
-
-for app in oRest:
-    YEBO_APPS += (app, )
-
-INSTALLED_APPS += YEBO_APPS
-
-try:
-    from .local import *
-except ImportError:
-    pass
+from YBAQNEXT.settings import *
+from YBAQNEXT.yeboapps import *
+from .local import *
